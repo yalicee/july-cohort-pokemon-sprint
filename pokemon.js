b7b0b4bfaaa3b6284ed9c424ae5abf87a1815ec7
Alex Yang <alexyang@Alexs-MacBook-Pro.local>
2022-08-11 17:02:24 +0100

    implement type classes

--- pokemon.js
+++ pokemon.js
@@ -1,5 +1,5 @@
 class Pokemon {
-  constructor(name, type = 'normal', hitPoints, attackDamage, move = 'tackle') {
+  constructor(name, type = "normal", hitPoints, attackDamage, move = "tackle") {
     this.name = name;
     this.type = type;
     this.hitPoints = hitPoints;
@@ -8,15 +8,45 @@ class Pokemon {
   }
 
   isEffectiveAgainst(pokemon) {
-    if (this.type === 'normal') {
-      return false;
-    } else {
-      return true;
+    // if (this.type === "normal") {
+    //   return false;
+    // } else if (this.type === "grass" && pokemon.type === "water") {
+    //   return true;
+    // } else if (this.type === "grass" && pokemon.type === "fire") {
+    //   return false;
+    // } else {
+    //   return true;
+    // }
+
+    const guide = {};
+
+    switch (this.type) {
+      case "normal":
+        guide["water"] = false;
+        guide["fire"] = false;
+        guide["grass"] = false;
+        break;
+      case "grass":
+        guide["water"] = true;
+        guide["fire"] = false;
+        guide["grass"] = false;
+        break;
+      case "water":
+        guide["fire"] = true;
+        guide["grass"] = false;
+        guide["water"] = false;
+        break;
+      case "fire":
+        guide["grass"] = true;
+        guide["water"] = false;
+        guide["fire"] = fire;
+        break;
     }
+    return guide[pokemon.type];
   }
 
   isWeakTo(pokemon) {
-    if (this.type === 'normal') {
+    if (this.type === "normal") {
       return false;
     } else {
       return true;
@@ -37,4 +67,22 @@ class Pokemon {
   }
 }
 
-module.exports = { Pokemon };
+class FirePokemon extends Pokemon {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, "fire", hitPoints, attackDamage, move);
+  }
+}
+
+class WaterPokemon extends Pokemon {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, "water", hitPoints, attackDamage, move);
+  }
+}
+
+class GrassPokemon extends Pokemon {
+  constructor(name, hitPoints, attackDamage, move) {
+    super(name, "grass", hitPoints, attackDamage, move);
+  }
+}
+
+module.exports = { Pokemon, FirePokemon, WaterPokemon, GrassPokemon };
